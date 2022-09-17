f2fa67c06d14104d61bca48d00ef08b6618ca575
uchastnik.7.00@mail.ru <uchastnik.7.00@mail.ru>
2022-09-17 13:26:59 +0300

    Progect 05 first commit

---
+++ project_05/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project_05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToInt32(input.Text);
+            double resulttmp = Convert.ToInt32(Math.Pow(2, a) - 1);
+            result.Text = Convert.ToString(resulttmp);
+        }
+    }
+}
