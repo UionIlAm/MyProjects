417479e26b722989e26cd8e41c3b833aa6514986
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-09-25 20:45:10 +0300

    Add files via upload

---
+++ project_08/CustomControl1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project_08
+{
+    /// <summary>
+    /// Выполните шаги 1a или 1b, а затем 2, чтобы использовать этот пользовательский элемент управления в файле XAML.
+    ///
+    /// Шаг 1a. Использование пользовательского элемента управления в файле XAML, существующем в текущем проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:project_08"
+    ///
+    ///
+    /// Шаг 1б. Использование пользовательского элемента управления в файле XAML, существующем в другом проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:project_08;assembly=project_08"
+    ///
+    /// Потребуется также добавить ссылку из проекта, в котором находится файл XAML,
+    /// на данный проект и пересобрать во избежание ошибок компиляции:
+    ///
+    ///     Щелкните правой кнопкой мыши нужный проект в обозревателе решений и выберите
+    ///     "Добавить ссылку"->"Проекты"->[Поиск и выбор проекта]
+    ///
+    ///
+    /// Шаг 2)
+    /// Теперь можно использовать элемент управления в файле XAML.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class CustomControl1 : Control
+    {
+
+        public class NewLine : FrameworkElement
+        {
+            public NewLine()
+            {
+                Height = 0;
+                var binding = new Binding
+                {
+                    RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(WrapPanel), 1),
+                    Path = new PropertyPath("ActualWidth")
+                };
+                BindingOperations.SetBinding(this, WidthProperty, binding);
+            }
+
+
+
+        }
+    }
+}
+
---
+++ project_08/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace project_08
+{
+
+    public partial class MainWindow : Window
+    {
+        private string resultMath = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int x1 = Convert.ToInt32(λ1.Text);
+                int x2 = Convert.ToInt32(λ2.Text);
+                int h1 = Convert.ToInt32(h.Text);
+                result.Text += Tab(x1, x2, h1);    
+            }
+            catch (Exception)
+            {
+                string message = "Должны быть целочисленые числа";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                string caption = "Ошибка";
+                result = System.Windows.Forms.MessageBox.Show(message, caption, buttons);
+            }
+        }
+        public string Tab(int λ1, int λ2, int h)
+        {
+            double[] dataX = new double[] { };
+            double[] dataY = new double[] { };
+            Array.Resize(ref dataX, λ2);
+            Array.Resize(ref dataY, λ2);
+            
+            for (int t = λ1; t < λ2 + 1; t++)
+            {
+                λ1 = +h;
+                dataX[λ1] = λ1;
+                double res = Math.Log10(t + Math.Abs((t * t) + 4)) - Math.Log10(t + Math.Sqrt((t * t) + 1));
+                resultMath += ($"{t}" + " = " + res.ToString() + "\n");
+                dataY[λ1] = res;
+            }
+            dataX = dataX.Where(a => a != 0).ToArray();
+            dataY = dataY.Where(a => a != 0).ToArray();
+            result_Graf.Plot.AddScatter(dataX, dataY);
+            result_Graf.Plot.SaveFig("WpfGraf.png");
+            result_Graf.Refresh();
+            return resultMath;
+        }
+    }
+}
