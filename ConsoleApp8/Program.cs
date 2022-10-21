12d6adc2a0eeafa5c94bcc84321ad0833f9d81fa
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:46:18 +0300

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число X ");
+            int X = Convert.ToInt32(Console.ReadLine());
+            double res = 0;
+            if (X > 0)
+            {
+                res = Math.Sin(X * X);
+                Console.WriteLine(res);
+            }
+            else
+            {
+                res = 1 + 2 * Math.Sin(X);
+                Console.WriteLine(res); 
+            }
+        }
+    }
+}
