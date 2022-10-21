9bf481c326c8cf97f4bd68407a84f2a61d7f3997
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:48:25 +0300

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace lb
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите число месяца ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 3)
+            {
+                Console.WriteLine("Весна");
+            }
+            if (a == 4)
+            {
+                Console.WriteLine("Весна");
+            }
+            if (a == 5)
+            {
+                Console.WriteLine("Весна");
+            }
+            if (a == 6)
+            {
+                Console.WriteLine("Лето");
+            }
+            if (a == 7)
+            {
+                Console.WriteLine("Лето");
+            }
+            if (a == 8)
+            {
+                Console.WriteLine("Лето");
+            }
+            else
+            {
+                Console.WriteLine("Cентябрь");
+            }
+        }
+
+    }
+}
+
