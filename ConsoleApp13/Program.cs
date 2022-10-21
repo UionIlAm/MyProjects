3133118e1fd982b2a8fe0ce6ad205370feaf284a
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:48:51 +0300

    Add files via upload

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace lb
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите стоимость 1кг конфет ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 21; i++)
+            {
+                Console.WriteLine($"{i}00г цена {a * i * 0.1}");
+            }
+        }
+
+    }
+}
+
