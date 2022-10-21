f025734e7d5afe805454fe2e4bc569a9e0afd6a8
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:45:14 +0300

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool A = true;
+            bool B = false;
+            bool C = false;
+            Console.WriteLine($"a){!A & B}");
+            Console.WriteLine($"б){A | !B}");
+            Console.WriteLine($"в){A & B | C}");
+        }
+    }
+}
