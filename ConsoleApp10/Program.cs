6b9e068208dd9bd2261a29dbc2d44130de6df802
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:47:20 +0300

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace lb
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите число a ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число b ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число c ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            if (a < b && b < c)
+                Console.WriteLine("a) True");
+            else
+                Console.WriteLine("False");
+            if (b > a && a > c)
+                Console.WriteLine("б) True");
+            else
+                Console.WriteLine("False");
+        }
+
+    }
+}
+
