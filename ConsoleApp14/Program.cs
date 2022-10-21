fa5b560e674fc07fae48857be877f735ae43e575
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:49:17 +0300

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lb
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите баллы за 1 экз ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите баллы за 2 экз ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите баллы за 3 экз ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите баллы за 4 экз ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            Console.WriteLine(sum = a + b + c + d);
+        }
+
+    }
+}
+
