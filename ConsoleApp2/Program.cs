331c8f67397d28c4ce442400e9c8ba9de444930c
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:42:07 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите расстояние в метрах");
+            double m = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{Math.Round(m / 1000, 0, MidpointRounding.ToPositiveInfinity)}");
+        }
+    }
+}
