6b6bf3c0184fd2de76dc84f6b5a40416622206a8
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:44:39 +0300

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{Math.Round(Math.PI, 2)}");
+        }
+    }
+}
