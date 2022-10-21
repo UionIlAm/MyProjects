ea4ab006858e1e28fa7c9e37d102ae506e545501
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:41:27 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Сторона А");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Сторона B");
+            double B = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"P = {2 * (A + B)}");
+            Console.WriteLine($"Длина диоганали: {Math.Sqrt((A * A) + ((B * B)))}");
+        }
+    }
+}
