fb037d9f871d6e5b879e7609017826538015c51e
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:43:59 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите трех значное число n");
+            string num = Console.ReadLine();
+            List<int> asd = new List<int>();
+            for (int i = 0; i < num.Length; i++)
+                if (int.TryParse(num[i].ToString(), out int res) && asd.Count <= 3)
+                    asd.Add(res);
+
+
+            Console.WriteLine($"число х: {asd[1]}{asd[2]}{asd[0]}");
+        }
+    }
+}
