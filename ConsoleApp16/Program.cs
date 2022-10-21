bc44855348579fdcfd70af234bc8c24c9e50784a
Илья <113088070+UionIlAm@users.noreply.github.com>
2022-10-22 00:50:03 +0300

    Add files via upload

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace lb
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 100; i < 200; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+}
+
